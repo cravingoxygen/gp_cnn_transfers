9df8272fe1c4e8b42c6286533efcb08130ee3d97
Elre Oldewage <vze.ezv@gmail.com>
2019-08-12 17:57:09 +0100

    Removed some old code that was related to the initialization/prior stuff

--- cnn.py
+++ cnn.py
@@ -131,25 +131,6 @@ class MNIST_arch_0(nn.Module):
     return F.log_softmax(x, dim=-1)
 
 
-def fc_uniform_init(fc_layer):
-  print('FC layer constant: {}'.format(fc_layer.weight.size(1)))
-  stdev = 1./math.sqrt(fc_layer.weight.size(1))
-  fc_layer.weight.data.uniform_(-stdev, stdev)
-  if fc_layer.bias is not None:
-    fc_layer.bias.data.uniform_(-stdev, stdev)
-  return fc_layer
-
-def conv2d_uniform_init(conv2d_layer):
-  n = conv2d_layer.in_channels
-  for k in conv2d_layer.kernel_size:
-    n *= k
-  print('conv layer constant: {}'.format(n))
-  stdev = 1./math.sqrt(n)
-  conv2d_layer.weight.data.uniform_(-stdev, stdev)
-  if conv2d_layer.bias is not None:
-    conv2d_layer.bias.data.uniform_(-stdev, stdev)
-  return conv2d_layer
-
 def fc_gaussian_init(fc_layer):
   print('FC layer constant: {}'.format(fc_layer.weight.size(1)))
   stdev = 1./math.sqrt(fc_layer.weight.size(1))
@@ -169,34 +150,7 @@ def conv2d_gaussian_init(conv2d_layer):
     conv2d_layer.bias.data.normal_(0, stdev)
   return conv2d_layer
 
-
-def test_pdf(num_samples=10000):
-    test_loader = torch.utils.data.DataLoader(
-      filter_dataset(datasets.MNIST('data', train=False, transform=transforms.ToTensor()), 2, 7),
-      batch_size=1, shuffle=False)
-    all_preds = np.zeros(num_samples)
-    
-    for k in range(0, num_samples):
-        torch_model = MNIST_arch_0()
-        if torch.cuda.is_available():
-            torch_model = torch_model.cuda()
-        #Use the data loader to iterate, but break after the first pattern.
-        for xs, ys in test_loader:
-            if torch.cuda.is_available():
-                xs, ys = xs.cuda(), ys.cuda()
-            preds = torch_model(xs)
-            preds_np = preds.data.cpu().numpy()
-            all_preds[k] = preds_np[0][0]
-            break
-    #all_preds = np.exp(all_preds)
-    plt.hist(all_preds, 100)
-    import pdb; pdb.set_trace()
-    plt.savefig("sanity_check_preds_1000_lnsoftmax.png".format(r), format='png', dpi=1200)
-    plt.close()
-    np.save('sanity_check_preds_1000_lnsoftmax',all_preds,)
-    
-
-def mnist_tutorial(nb_epochs=NB_EPOCHS, batch_size=BATCH_SIZE,
+def mnist(nb_epochs=NB_EPOCHS, batch_size=BATCH_SIZE,
                    train_end=-1, test_end=-1, learning_rate=LEARNING_RATE):
   """
   MNIST cleverhans tutorial
@@ -364,68 +318,9 @@ def mnist_tutorial(nb_epochs=NB_EPOCHS, batch_size=BATCH_SIZE,
     b += 1
   
   np.save('/scratch/etv21/conv_gp_data/MNIST_data/expA/two_vs_seven_adv_{}'.format(attack_name), adv_images, allow_pickle=False)
-  #np.save('/scratch/etv21/conv_gp_data/MNIST_data/expA/two_vs_seven_test', clean_images, allow_pickle=False)
   np.save('/scratch/etv21/conv_gp_data/MNIST_data/expA/two_vs_seven_labels', adv_labels, allow_pickle=False)
   
   return report
-  
-def parse_val(cnns_list, key):
-  
-  item_list = torch.zeros((cnns_list.size, cnns_list[0][key].shape[0]))
-  
-  #print('Forming list {}'.format(key))
-  for n in range(0,cnns_list.size):
-    item_list[n] = cnns_list[n][key]
-  #print('Moving to gpu')
-  #item_list = item_list.to(torch.device("cuda"))
-  plt.hist(item_list.flatten(), 100)
-  plt.savefig("{}_{}.png".format(key, cnns_list.size), format='png', dpi=100)
-  plt.close()
-  
-  #print('Calculating mean')
-  list_mean = item_list.mean().item()
-  #print('Calculating variance')
-  list_var = item_list.var().item()
-  #print('Layer\tMean\tVariance')  
-  print('{}:\t{}\t{}'.format(key, list_mean, list_var))
-  return list_mean, list_var
-
-def load_cnns():
-  num_runs = 100
-  cnns = np.empty(num_runs,dtype=object)
-  
-  for n in tqdm.trange(0,num_runs):
-    with open("/scratch3/etv21/cnn_weights_biases/cnn_{}".format(n), 'rb') as f:
-      cnns[n] = pickle.load(f)
-      if n == 0:
-        cnns[n]['cn1w'] = cnns[n]['cn1w'].to(torch.device("cuda"))
-  
-  print('Layer\tMean\tVariance')
-  parse_val(cnns, 'cn1w')
-  parse_val(cnns, 'cn1b')
-  parse_val(cnns, 'cn2w')
-  parse_val(cnns, 'cn2b')
-  parse_val(cnns, 'fc1w')
-  parse_val(cnns, 'fc1b')
-  
-def train_cnns():
-  num_runs = 10000
-  
-  accuracies = np.zeros(num_runs)
-  cnns = np.empty(num_runs,dtype=object)
-  
-  for n in tqdm.trange(0,num_runs):
-    test_acc, torch_model = mnist_tutorial(nb_epochs=FLAGS.nb_epochs,batch_size=FLAGS.batch_size,learning_rate=FLAGS.learning_rate)
-    accuracies[n] = test_acc
-    cn1w, cn1b = torch_model.conv1.weight.detach().flatten().cpu(), torch_model.conv1.bias.detach().flatten().cpu()
-    cn2w, cn2b = torch_model.conv2.weight.detach().flatten().cpu(), torch_model.conv2.bias.detach().flatten().cpu()
-    fc1w, fc1b = torch_model.fc1.weight.detach().flatten().cpu(), torch_model.fc1.bias.detach().flatten().cpu()
-    cnn_dict = {"cn1w": cn1w, "cn1b": cn1b, "cn2w":cn2w, "cn2b": cn2b, "fc1w": fc1w, "fc1b": fc1b}
-    with open("/scratch3/etv21/cnn_weights_biases/cnn_{}".format(n), 'wb') as f:
-      pickle.dump(cnn_dict, f)
-    cnns[n] = cnn_dict
-    
-  np.save('/scratch3/etv21/cnn_weights_biases/accuracies_{}_runs'.format(num_runs), accuracies)
   
 
 def main(_=None):
@@ -434,7 +329,7 @@ def main(_=None):
   #train_cnns()
   #load_cnns()
   
-  mnist_tutorial(nb_epochs=FLAGS.nb_epochs,batch_size=FLAGS.batch_size,learning_rate=FLAGS.learning_rate)
+  mnist(nb_epochs=FLAGS.nb_epochs,batch_size=FLAGS.batch_size,learning_rate=FLAGS.learning_rate)
   
   #test_pdf()
 
@@ -447,87 +342,3 @@ if __name__ == '__main__':
                      'Learning rate for training')
 
   tf.app.run()
-
-
-
-'''
-def filter_dataset(dataset, class_one, class_two):
-    if dataset.train:
-        images = dataset.train_data
-        labels = dataset.train_labels
-    else:
-        images = dataset.test_data
-        labels = dataset.test_labels
-
-    class_one_mask = labels == class_one
-    class_two_mask = labels == class_two
-    extracted_images = torch.cat((images[class_one_mask], images[class_two_mask]))
-    
-    #Convert labels into 2-class labels:
-    #one_hot_c1, one_hot_c2 = torch.tensor([1,0]), torch.tensor([0,1])
-    #xtracted_labels = torch.cat((one_hot_c1.repeat(class_one_mask.sum(), 1), one_hot_c2.repeat(class_two_mask.sum(), 1)))
-    extracted_labels = torch.cat((labels[class_one_mask]*0, labels[class_two_mask]/class_two))
-    
-    if dataset.train:
-        dataset.train_data = extracted_images
-        dataset.train_labels = extracted_labels
-    else:
-        dataset.test_data = extracted_images
-        dataset.test_labels = extracted_labels
-        
-    return dataset
-'''
-
-def examine_weights_biases(torch_model):
-  cn1_weights, cn1_bias = torch_model.conv1.weight.detach(), torch_model.conv1.bias.detach()
-  cn2_weights, cn2_bias = torch_model.conv2.weight.detach(), torch_model.conv2.bias.detach()
-  fc1_weights, fc1_bias = torch_model.fc1.weight.detach(), torch_model.fc1.bias.detach()
-  all_weights, all_bias = torch.cat([cn1_weights.flatten(), cn2_weights.flatten(), fc1_weights.flatten()]), torch.cat([cn1_bias.flatten(), cn2_bias.flatten(), fc1_bias.flatten()])
-  
-  cn1_weights_mean, cn1_weights_var = torch.mean(cn1_weights), torch.var(cn1_weights)
-  cn2_weights_mean, cn2_weights_var = torch.mean(cn2_weights), torch.var(cn2_weights)
-  fc1_weights_mean, fc1_weights_var = torch.mean(fc1_weights), torch.var(fc1_weights)
-  
-  cn1_bias_mean, cn1_bias_var = torch.mean(cn1_bias), torch.var(cn1_bias)
-  cn2_bias_mean, cn2_bias_var = torch.mean(cn2_bias), torch.var(cn2_bias)
-  fc1_bias_mean, fc1_bias_var = torch.mean(fc1_bias), torch.var(fc1_bias)
-  
-  
-  '''
-  print("ConvLayer 1 weights: mean, variance = {}, {}".format(cn1_weights_mean, cn1_weights_var))
-  print("ConvLayer 2 weights: mean, variance = {}, {}".format(cn2_weights_mean, cn2_weights_var))
-  print("FC 1 weights: mean, variance = {}, {}".format(fc1_weights_mean, fc1_weights_var))
-
-  print("ConvLayer 1 bias: mean, variance = {}, {}".format(cn1_bias_mean, cn1_bias_var))
-  print("ConvLayer 2 bias: mean, variance = {}, {}".format(cn2_bias_mean, cn2_bias_var))
-  print("FC 1 bias: mean, variance = {}, {}".format(fc1_bias_mean, fc1_bias_var))
-  
-  print("Overall weight mean and variance: {}, {}".format(torch.mean(all_weights), torch.var(all_weights)))
-  print("Overall bias mean and variance: {}, {}".format(torch.mean(all_bias), torch.var(all_bias)))
-  plt.hist(cn1_weights.flatten().cpu(), 100)
-  plt.savefig("cn1_weights_{}.png".format(r), format='png', dpi=1200)
-  plt.close()
-  plt.hist(cn1_bias.flatten().cpu(), 100)
-  plt.savefig("cn1_bias_{}.png".format(r), format='png', dpi=1200)
-  plt.close()
-  
-  plt.hist(cn2_weights.flatten().cpu(), 100)
-  plt.savefig("cn2_weights_{}.png".format(r), format='png', dpi=1200)
-  plt.close()
-  plt.hist(cn2_bias.flatten().cpu(), 100)
-  plt.savefig("cn2_bias_{}.png".format(r), format='png', dpi=1200)
-  plt.close()
-  
-  plt.hist(fc1_weights.flatten().cpu(), 100)
-  plt.savefig("fc1_weights_{}.png".format(r), format='png', dpi=1200)
-  plt.close()
-  plt.hist(fc1_bias.flatten().cpu(), 100)
-  plt.savefig("fc1_bias_{}.png".format(r), format='png', dpi=1200)
-  plt.close()
-  plt.hist(all_weights.cpu(), 100)
-  plt.savefig("all_weights_{}.png".format(r), format='png', dpi=1200)
-  plt.close()
-  plt.hist(all_bias.cpu(), 100)
-  plt.savefig("all_bias_{}.png".format(r), format='png', dpi=1200)
-  plt.close()
-  '''
